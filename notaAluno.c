1e728641e5b2a6d9513872ce9b5700fb4d3eee5f
Paulo Rodrigues <116692363+paulorodriguesfelix@users.noreply.github.com>
2023-03-30 16:52:18 -0300

    Add files via upload

---
+++ notaAluno.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+float pesoN1(float n){
+   float result = n*40/100;
+    return result;
+}
+float pesoN2(float n){
+    float result = n*60/100;
+    return result;
+    
+}
+
+int main()
+{
+    float n1;
+    float n2;
+    float notaFinal;
+    printf("==CALCULA NOTA==\n\n");
+    printf("Digite o valor da primeira nota: ");
+    scanf("%f",&n1);
+    printf("Digite o valor da segunda nota: ");
+    scanf("%f",&n2);
+    float result1 = pesoN1(n1);
+    float result2 = pesoN2(n2);
+    notaFinal = result1+result2;
+    printf("Peso nota Nº 1: %.1f\n",result1);
+    printf("Peso nota Nº 2: %.1f\n",result2);
+    printf("Nota final: %.1f\n", notaFinal);
+    if(notaFinal <= 4.9 ){
+        printf("Condeito final: D");
+    }else if(notaFinal >= 5 && notaFinal <= 6.9){
+        printf("Condeito final: C");
+    }else if(notaFinal >= 7 && notaFinal <=8.9){
+        printf("Condeito final: B");
+    }else{
+        printf("Condeito final: A");
+    }
+    return 0;
+    
+}
